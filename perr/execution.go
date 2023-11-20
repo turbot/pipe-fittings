1a7fbd7e8582d16c0bc1b182f2424b3aea6ce6b9
Victor Hadianto <victor@turbot.com>
2023-11-21 09:23:56 +1100

    Added perr 460 and 461.

---
+++ perr/execution.go
@@ -0,0 +1,43 @@
+package perr
+
+import (
+	"fmt"
+)
+
+const (
+	ErrorCodeExecutionError = "error_execution"
+	StatusExecutionError    = 460
+)
+
+func ExecutionError(itemType string, id string) ErrorModel {
+	e := ErrorModel{
+		Instance: reference(),
+		Type:     ErrorCodeExecutionError,
+		Title:    "Execution Error",
+		Status:   StatusExecutionError,
+	}
+	if id != "" {
+		e.Detail = fmt.Sprintf("%s = %s.", itemType, id)
+	}
+	return e
+}
+
+func ExecutionErrorWithMessage(msg string) ErrorModel {
+	return ExecutionErrorWithTypeAndMessage(ErrorCodeExecutionError, msg)
+}
+
+func ExecutionErrorWithTypeAndMessage(errorType string, msg string) ErrorModel {
+	e := ErrorModel{
+		Instance: reference(),
+		Type:     errorType,
+		Title:    "Bad Request",
+		Status:   StatusExecutionError,
+		Detail:   msg,
+	}
+	return e
+}
+
+func IsExecutionError(err error) bool {
+	e, ok := err.(ErrorModel)
+	return ok && e.Status == StatusExecutionError
+}
