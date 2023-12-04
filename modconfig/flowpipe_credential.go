900829a307e9d2ca0322de7065f8c4dfd53ad605
Subhajit Mondal <subhajit@turbot.com>
2023-12-04 12:10:21 +0530

    Add `Pipes` credential type. Closes #67 (#83)

--- modconfig/flowpipe_credential.go
+++ modconfig/flowpipe_credential.go
@@ -37,7 +37,7 @@ type AwsCredential struct {
 }
 
 func DefaultCredentialNames() []string {
-	return []string{"aws.default", "slack.default", "basic.default", "gcp.default", "abuseipdb.default", "sendgrid.default", "virustotal.default", "zendesk.default", "trello.default", "okta.default", "uptimerobot.default", "urlscan.default", "clickup.default", "pagerduty.default", "discord.default", "ip2location.default", "ipstack.default", "teams.default", "aws.<dynamic>", "slack.<dynamic>", "basic.<dynamic>", "gcp.<dynamic>", "abuseipdb.<dynamic>", "sendgrid.<dynamic>", "virustotal.<dynamic>", "zendesk.<dynamic>", "trello.<dynamic>", "okta.<dynamic>", "uptimerobot.<dynamic>", "urlscan.<dynamic>", "clickup.<dynamic>", "pagerduty.<dynamic>", "discord.<dynamic>", "ip2location.<dynamic>", "ipstack.<dynamic>", "teams.<dynamic>"}
+	return []string{"aws.default", "slack.default", "basic.default", "gcp.default", "abuseipdb.default", "sendgrid.default", "virustotal.default", "zendesk.default", "trello.default", "okta.default", "uptimerobot.default", "urlscan.default", "clickup.default", "pagerduty.default", "discord.default", "ip2location.default", "ipstack.default", "teams.default", "pipes.default", "aws.<dynamic>", "slack.<dynamic>", "basic.<dynamic>", "gcp.<dynamic>", "abuseipdb.<dynamic>", "sendgrid.<dynamic>", "virustotal.<dynamic>", "zendesk.<dynamic>", "trello.<dynamic>", "okta.<dynamic>", "uptimerobot.<dynamic>", "urlscan.<dynamic>", "clickup.<dynamic>", "pagerduty.<dynamic>", "discord.<dynamic>", "ip2location.<dynamic>", "ipstack.<dynamic>", "teams.<dynamic>", "pipes.<dynamic>"}
 }
 
 func (*AwsCredential) GetCredentialType() string {
@@ -1128,6 +1128,69 @@ func (c *MicrosoftTeamsCredential) GetTtl() int {
 }
 
 func (c *MicrosoftTeamsCredential) Validate() hcl.Diagnostics {
+	return hcl.Diagnostics{}
+}
+
+type PipesCredential struct {
+	HclResourceImpl
+	ResourceWithMetadataImpl
+
+	Type string `json:"type" cty:"type" hcl:"type,label"`
+
+	Token *string `json:"token,omitempty" cty:"token" hcl:"token,optional"`
+}
+
+func (*PipesCredential) GetCredentialType() string {
+	return "pipes"
+}
+
+func (c *PipesCredential) getEnv() map[string]cty.Value {
+	env := map[string]cty.Value{}
+	if c.Token != nil {
+		env["PIPES_TOKEN"] = cty.StringVal(*c.Token)
+	}
+	return env
+}
+
+func (c *PipesCredential) CtyValue() (cty.Value, error) {
+	ctyValue, err := GetCtyValue(c)
+	if err != nil {
+		return cty.NilVal, err
+	}
+
+	valueMap := ctyValue.AsValueMap()
+	valueMap["env"] = cty.ObjectVal(c.getEnv())
+
+	return cty.ObjectVal(valueMap), nil
+}
+
+func (c *PipesCredential) Resolve(ctx context.Context) (Credential, error) {
+	if c.Token == nil {
+		pipesTokenEnvVar := os.Getenv("PIPES_TOKEN")
+
+		// Don't modify existing credential, resolve to a new one
+		newCreds := &PipesCredential{
+			HclResourceImpl: HclResourceImpl{
+				FullName:        c.FullName,
+				UnqualifiedName: c.UnqualifiedName,
+				ShortName:       c.ShortName,
+				DeclRange:       c.DeclRange,
+				blockType:       c.blockType,
+			},
+			Type:  c.Type,
+			Token: &pipesTokenEnvVar,
+		}
+
+		return newCreds, nil
+	}
+	return c, nil
+}
+
+func (c *PipesCredential) GetTtl() int {
+	return -1
+}
+
+func (c *PipesCredential) Validate() hcl.Diagnostics {
 	return hcl.Diagnostics{}
 }
 
