dddbe09283870de468347bba8feb0c772a363844
kai <kai@turbot.com>
2023-12-05 13:48:20 +0000

    Fix GetConfigPaths

--- cmdconfig/workspace_profile.go
+++ cmdconfig/workspace_profile.go
@@ -46,7 +46,7 @@ func GetConfigPath() ([]string, error) {
 	if len(configPathArg) == 0 {
 		return nil, fmt.Errorf("no config path specified")
 	}
-	configPaths := strings.Split(viper.GetString(constants.ArgConfigPath), ":")
+	configPaths := strings.Split(configPathArg, ":")
 
 	for i, p := range configPaths {
 		// special case for "." - use the mod location
