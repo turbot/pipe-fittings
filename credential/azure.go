f6d9d46f33aecb0699c7c2b6415ef7fb2acad783
Subhajit Mondal <subhajit@turbot.com>
2024-02-22 20:13:35 +0530

    Add `Equals` functions for `AWS`, `Azure` and `GCP` credential (#288)

--- credential/azure.go
+++ credential/azure.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/turbot/pipe-fittings/modconfig"
+	"github.com/turbot/pipe-fittings/utils"
 	"github.com/zclconf/go-cty/cty"
 )
 
@@ -45,6 +46,35 @@ func (c *AzureCredential) CtyValue() (cty.Value, error) {
 	valueMap["env"] = cty.ObjectVal(c.getEnv())
 
 	return cty.ObjectVal(valueMap), nil
+}
+
+func (c *AzureCredential) Equals(other *AzureCredential) bool {
+	// If both pointers are nil, they are considered equal
+	if c == nil && other == nil {
+		return true
+	}
+
+	if (c == nil && other != nil) || (c != nil && other == nil) {
+		return false
+	}
+
+	if !utils.StringPtrEqual(c.ClientID, other.ClientID) {
+		return false
+	}
+
+	if !utils.StringPtrEqual(c.ClientSecret, other.ClientSecret) {
+		return false
+	}
+
+	if !utils.StringPtrEqual(c.Environment, other.Environment) {
+		return false
+	}
+
+	if !utils.StringPtrEqual(c.TenantID, other.TenantID) {
+		return false
+	}
+
+	return true
 }
 
 func (c *AzureCredential) Resolve(ctx context.Context) (Credential, error) {
