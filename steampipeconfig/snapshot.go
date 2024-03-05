cefdc967c680696d0f508d57edbdff10e4341f20
Mike Burgess <mike@turbot.com>
2024-03-05 21:56:27 +0000

    Ensure correct schema version for Steampipe snapshots

--- steampipeconfig/snapshot.go
+++ steampipeconfig/snapshot.go
@@ -7,7 +7,7 @@ import (
 	steampipecloud "github.com/turbot/steampipe-cloud-sdk-go"
 )
 
-var SteampipeSnapshotSchemaVersion int64 = 20221222
+var SteampipeSnapshotSchemaVersion int64 = 20240130
 
 type SteampipeSnapshot struct {
 	SchemaVersion string                   `json:"schema_version"`
