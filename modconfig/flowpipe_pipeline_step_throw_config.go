15258cb46e2cad43e490afa2d7d2480d367a42e2
Victor Hadianto <victor@turbot.com>
2024-03-08 11:28:25 +1100

    wip

--- modconfig/flowpipe_pipeline_step_throw_config.go
+++ modconfig/flowpipe_pipeline_step_throw_config.go
@@ -2,6 +2,9 @@ package modconfig
 
 import (
 	"github.com/hashicorp/hcl/v2"
+	"github.com/turbot/pipe-fittings/hclhelpers"
+	"github.com/turbot/pipe-fittings/schema"
+	"github.com/zclconf/go-cty/cty"
 )
 
 func NewThrowConfig(p *PipelineStepBase) *ThrowConfig {
@@ -18,4 +21,61 @@ type ThrowConfig struct {
 
 	If      *bool
 	Message *string
+}
+
+func (t *ThrowConfig) AppendDependsOn(dependsOn ...string) {
+	t.PipelineStepBase.AppendDependsOn(dependsOn...)
+}
+
+func (t *ThrowConfig) AppendCredentialDependsOn(...string) {
+	// not implemented
+}
+
+func (t *ThrowConfig) AddUnresolvedAttribute(name string, expr hcl.Expression) {
+	t.UnresolvedAttributes[name] = expr
+}
+
+func (t *ThrowConfig) SetAttributes(hclAttributes hcl.Attributes, evalContext *hcl.EvalContext) hcl.Diagnostics {
+	diags := hcl.Diagnostics{}
+
+	for name, attr := range hclAttributes {
+		switch name {
+		case schema.AttributeTypeIf:
+			t.AddUnresolvedAttribute(name, attr.Expr)
+		case schema.AttributeTypeMessage:
+			val, stepDiags := dependsOnFromExpressions(attr, evalContext, t)
+			if len(stepDiags) > 0 {
+				diags = append(diags, stepDiags...)
+				continue
+			}
+
+			if val != cty.NilVal {
+				valString, err := hclhelpers.CtyToString(val)
+				if err != nil {
+					diags = append(diags, &hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Unable to parse " + schema.AttributeTypeMessage + " attribute to string",
+						Subject:  &attr.Range,
+					})
+					continue
+				}
+
+				t.Message = &valString
+			}
+		default:
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  "Invalid attribute",
+				Detail:   "Unsupported attribute '" + name + "' in throw block",
+				Subject:  &attr.Range,
+			})
+		}
+	}
+
+	return diags
+}
+
+func (t *ThrowConfig) Resolve() *ThrowConfig {
+
+	return t
 }
