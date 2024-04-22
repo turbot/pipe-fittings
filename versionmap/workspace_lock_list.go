c5114b05b0efb72776465b1b5ec1cec183c7e901
kai <kai@turbot.com>
2024-04-22 14:56:53 +0100

    When installing/updating transitive depdencies, without updating/installing their parents, the tree display of installed mods is not rendered correctly

--- versionmap/workspace_lock_list.go
+++ versionmap/workspace_lock_list.go
@@ -1,7 +1,6 @@
 package versionmap
 
 func (l *WorkspaceLock) GetModList(rootName string) string {
-
-	tree := l.InstallCache.GetDependencyTree(rootName)
+	tree := l.InstallCache.GetDependencyTree(rootName, l)
 	return tree.String()
 }
