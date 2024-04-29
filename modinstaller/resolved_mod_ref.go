c40d237258949cf3769e5dd8233c76852431fe80
kai <kai@turbot.com>
2024-04-29 12:14:57 +0100

    midway through unifying data types

--- modinstaller/resolved_mod_ref.go
+++ modinstaller/resolved_mod_ref.go
@@ -2,13 +2,13 @@ package modinstaller
 
 import (
 	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/turbot/pipe-fittings/modconfig"
 	"github.com/turbot/pipe-fittings/versionhelpers"
 	"github.com/turbot/pipe-fittings/versionmap"
 )
 
 // ResolvedModRef is a struct to represent a resolved mod git reference
-type ResolvedModRef struct {
+
+type _ResolvedModRef struct {
 	// the FQN of the mod - also the Git URL of the mod repo
 	Name string
 	// the mod version
@@ -22,22 +22,23 @@ type ResolvedModRef struct {
 	GitReference *plumbing.Reference
 }
 
-func NewResolvedModRef(requiredModVersion *modconfig.ModVersionConstraint, version *versionmap.DependencyVersion) (*ResolvedModRef, error) {
-	res := &ResolvedModRef{
-		Name:       requiredModVersion.Name,
-		Version:    version,
-		Constraint: requiredModVersion.Constraint(),
-		FilePath:   requiredModVersion.FilePath(),
-		Branch:     requiredModVersion.Branch(),
-	}
-	if res.FilePath == "" {
-		res.GitReference = version.GitRef
-	}
-
-	return res, nil
-}
-
-// DependencyPath returns name in the format <dependency name>@v<dependencyVersion>
-func (r *ResolvedModRef) DependencyPath() string {
-	return modconfig.BuildModDependencyPath(r.Name, r.Version.Version)
-}
+//
+//func NewResolvedModRef(requiredModVersion *modconfig.ModVersionConstraint, version *versionmap.DependencyVersion) (*ResolvedModRef, error) {
+//	res := &ResolvedModRef{
+//		Name:       requiredModVersion.Name,
+//		Version:    version,
+//		Constraint: requiredModVersion.Constraint(),
+//		FilePath:   requiredModVersion.FilePath(),
+//		Branch:     requiredModVersion.Branch(),
+//	}
+//	if res.FilePath == "" {
+//		res.GitReference = version.GitRef
+//	}
+//
+//	return res, nil
+//}
+//
+//// DependencyPath returns name in the format <dependency name>@v<dependencyVersion>
+//func (r *ResolvedModRef) DependencyPath() string {
+//	return modconfig.BuildModDependencyPath(r.Name, r.Version.Version)
+//}
--- versionmap/resolved_version_constraint.go
+++ versionmap/resolved_version_constraint.go
@@ -1,31 +1,30 @@
 package versionmap
 
 import (
-	"github.com/Masterminds/semver/v3"
 	"github.com/turbot/pipe-fittings/modconfig"
 )
 
+// ResolvedVersionConstraint is a struct to represent a version constraint which has been resolved to specific version
+// (either a git tag, git commit (for a branch constraint) or a file location)
 type ResolvedVersionConstraint struct {
-	Name          string          `json:"name,omitempty"`
-	Alias         string          `json:"alias,omitempty"`
-	Constraint    string          `json:"constraint,omitempty"`
-	Version       *semver.Version `json:"version,omitempty"`
-	Branch        string          `json:"branch,omitempty"`
-	FilePath      string          `json:"file_path,omitempty"`
-	StructVersion int             `json:"struct_version,omitempty"`
-	Commit        string          `json:"commit,omitempty"`
-	GitRef        string          `json:"git_ref,omitempty"`
+	*DependencyVersion
+	Name          string `json:"name,omitempty"`
+	Alias         string `json:"alias,omitempty"`
+	Constraint    string `json:"constraint,omitempty"`
+	Commit        string `json:"commit,omitempty"`
+	GitRef        string `json:"git_ref,omitempty"`
+	StructVersion int    `json:"struct_version,omitempty"`
 }
 
-func NewResolvedVersionConstraint(name, alias string, version *semver.Version, constraintString string, gitRef, commit string) *ResolvedVersionConstraint {
+func NewResolvedVersionConstraint(version *DependencyVersion, name, alias string, constraintString string, gitRef, commit string) *ResolvedVersionConstraint {
 	return &ResolvedVersionConstraint{
-		Name:          name,
-		Alias:         alias,
-		Version:       version,
-		Constraint:    constraintString,
-		StructVersion: WorkspaceLockStructVersion,
-		Commit:        commit,
-		GitRef:        gitRef,
+		DependencyVersion: version,
+		Name:              name,
+		Alias:             alias,
+		Constraint:        constraintString,
+		Commit:            commit,
+		GitRef:            gitRef,
+		StructVersion:     WorkspaceLockStructVersion,
 	}
 }
 
