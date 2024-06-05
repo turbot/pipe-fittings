130faaed1e638133dccf80445325bb5b71e5b2f0
Puskar Basu <45908484+pskrbasu@users.noreply.github.com>
2024-06-05 15:09:07 +0530

    Add UniqueNameGenerator functions to generate random unique column names. Closes #417

--- utils/string.go
+++ utils/string.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"crypto/rand"
+	"fmt"
+	"strings"
 	"unicode"
 	"unicode/utf8"
 
@@ -31,4 +34,60 @@ func CapitalizeFirst(s string) string {
 	}
 	r, size := utf8.DecodeRuneInString(s)
 	return string(unicode.ToUpper(r)) + s[size:]
+}
+
+type UniqueNameGenerator struct {
+	lookup map[string]struct{}
+}
+
+// ctor
+func NewUniqueNameGenerator() *UniqueNameGenerator {
+	return &UniqueNameGenerator{
+		lookup: make(map[string]struct{}),
+	}
+}
+
+// randomString generates a random lowercase string of length n.
+func (g *UniqueNameGenerator) randomString(n int) string {
+	const alphabet = "abcdefghijklmnopqrstuvwxyz"
+	if n <= 0 {
+		return ""
+	}
+
+	// Generate n random bytes
+	randomBytes := make([]byte, n)
+	if _, err := rand.Read(randomBytes); err != nil {
+		return ""
+	}
+
+	// Map each random byte to a character in the alphabet
+	var sb strings.Builder
+	for _, b := range randomBytes {
+		sb.WriteByte(alphabet[int(b)%len(alphabet)])
+	}
+
+	return sb.String()
+}
+
+// GetUniqueName returns a unique name based on the input name
+// If the input name is not unique, a random lowercase string is appended to the name
+// This is used in steampipe and powerpipe to ensure unique column names in JSON output
+// when same columns are requested.
+func (g *UniqueNameGenerator) GetUniqueName(name string) string {
+	// store the original name
+	originalName := name
+
+	// ensure a unique column name
+	for {
+		// check the lookup to see if this name exists
+		if _, exists := g.lookup[name]; !exists {
+			// name is unique - we are done
+			break
+		}
+		// name is not unique - generate a new name
+		name = fmt.Sprintf("%s_%s", originalName, g.randomString(4))
+	}
+	// add the unique name into the lookup
+	g.lookup[name] = struct{}{}
+	return name
 }
