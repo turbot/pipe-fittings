95505884b25b50e991e8a364b8e53e5796223b55
kai <kai@turbot.com>
2024-06-06 10:12:59 +0100

    reverse unique name logic to have unique_name property

--- queryresult/column_def.go
+++ queryresult/column_def.go
@@ -4,10 +4,10 @@ import "reflect"
 
 // ColumnDef is a struct used to store column information from query results
 type ColumnDef struct {
-	Name         string `json:"name"`
-	DataType     string `json:"data_type"`
-	OriginalName string `json:"original_name,omitempty"`
-	isScalar     *bool
+	Name       string `json:"name"`
+	DataType   string `json:"data_type"`
+	UniqueName string `json:"unique_name,omitempty"`
+	isScalar   *bool
 }
 
 // IsScalar checks if the given value is a scalar value
@@ -25,4 +25,15 @@ func (c *ColumnDef) IsScalar(v any) bool {
 		c.isScalar = &scalar
 	}
 	return *c.isScalar
+}
+
+// GetUniqueName returns the unique name of the column
+func (c *ColumnDef) GetUniqueName() string {
+	// if UniqueName is set, that indicates the original column name exists more than once in a row
+	// so we have generated a unique name for it. Return this unique name
+	if c.UniqueName != "" {
+		return c.UniqueName
+	}
+	// otherwise return the original column name
+	return c.Name
 }
