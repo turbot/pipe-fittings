aa239ffea03e7cb1fb6df42680ec7ecb02e4ac0f
Mike Burgess <mike@turbot.com>
2024-06-10 15:48:22 +0100

    Unique column name generator should take hash using the column index as an input rather than appending occurrence index to the hashed output. Closes #426.

--- utils/unique_name_generator.go
+++ utils/unique_name_generator.go
@@ -6,13 +6,13 @@ import (
 
 type UniqueNameGenerator struct {
 	// lookup storing how many times each name has been used
-	lookup map[string]int
+	lookup map[string]struct{}
 }
 
 // NewUniqueNameGenerator creates a new UniqueNameGenerator
 func NewUniqueNameGenerator() *UniqueNameGenerator {
 	return &UniqueNameGenerator{
-		lookup: make(map[string]int),
+		lookup: make(map[string]struct{}),
 	}
 }
 
@@ -20,25 +20,23 @@ func NewUniqueNameGenerator() *UniqueNameGenerator {
 // If the input name is not unique, a random lowercase string is appended to the name
 // This is used in steampipe and powerpipe to ensure unique column names in JSON output
 // when same columns are requested.
-func (g *UniqueNameGenerator) GetUniqueName(name string) (string, error) {
+func (g *UniqueNameGenerator) GetUniqueName(name string, columnIndex int) (string, error) {
 	uniqueName := name
 
-	// check the lookup to see if this name exists
-	nameCount := g.lookup[name]
-	if nameCount != 0 {
-		// name is not unique - generate a new name using the hash of the name and the name count
-		hash, err := Base36Hash(name, 4)
+	// check the lookup to see if this column name already exists
+	_, isDuplicate := g.lookup[name]
+	if isDuplicate {
+		// name is not unique - generate a new name using the hash of the name and the column index
+		hash, err := Base36Hash(fmt.Sprintf("%s%d", name, columnIndex), 4)
 		if err != nil {
 			return "", err
 		}
 
-		uniqueName = fmt.Sprintf("%s_%s%d", name, hash, nameCount)
+		uniqueName = fmt.Sprintf("%s_%s", name, hash)
 	}
 
-	// increment the name count
-	nameCount++
 	// store the count for the original name in the lookup
-	g.lookup[name] = nameCount
+	g.lookup[name] = struct{}{}
 
 	return uniqueName, nil
 }
