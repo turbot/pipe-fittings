977efd19ae08267c43f210f2385072cb43ecb10f
Victor Hadianto <victor@turbot.com>
2024-08-01 09:35:50 +1000

    Added JSONTime type.

--- utils/time.go
+++ utils/time.go
@@ -1,6 +1,11 @@
 package utils
 
-import "time"
+import (
+	"database/sql/driver"
+	"fmt"
+	"strings"
+	"time"
+)
 
 const RFC3339WithMS = "2006-01-02T15:04:05.000Z07:00"
 
@@ -12,4 +17,48 @@ func TimePtr(t time.Time) *time.Time {
 // TimeNowPtr returns a pointer to the current time in UTC.
 func TimeNow() *time.Time {
 	return TimePtr(time.Now().UTC())
+}
+
+// JSONTime is a timestamp that can be serialized into JSON format
+type JSONTime struct {
+	time.Time
+}
+
+// MarshalJSON converts a JSONTime struct into a JSON byte buf
+func (t *JSONTime) MarshalJSON() ([]byte, error) {
+	if t.IsZero() {
+		return []byte("null"), nil
+	}
+	ts := fmt.Sprintf("\"%s\"", t.Format(RFC3339WithMS))
+	return []byte(ts), nil
+}
+
+// UnmarshalJSON converts buf into a JSONTime struct
+func (t *JSONTime) UnmarshalJSON(buf []byte) error {
+	s := string(buf)
+	// If it's set to null, then return nil for empty / null
+	if s == "null" {
+		return nil
+	}
+	// Otherwise, assume it's likely a datetime string (quoted in JSON)
+	rawStr := strings.Trim(string(buf), `"`)
+	bt, err := time.Parse(RFC3339WithMS, rawStr)
+	if err != nil {
+		// oops ... couldn't parse the time
+		return err
+	}
+	t.Time = bt
+	return nil
+}
+
+// https://stackoverflow.com/questions/61630216/converting-time-from-db-to-custom-time-fails
+func (j *JSONTime) Scan(src interface{}) error {
+	if t, ok := src.(time.Time); ok {
+		j.Time = t.UTC()
+	}
+	return nil
+}
+
+func (j JSONTime) Value() (driver.Value, error) {
+	return j.Time, nil
 }
