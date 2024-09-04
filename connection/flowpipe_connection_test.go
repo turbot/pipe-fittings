d61e7c428c241f206acb40f5531e0e6d0e220b8f
Omero Saienni <omero@turbot.com>
2024-09-05 11:54:19 +1200

    Convert credential for abuseipdb to connection

--- connection/flowpipe_connection_test.go
+++ connection/flowpipe_connection_test.go
@@ -6,7 +6,35 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/turbot/pipe-fittings/modconfig"
 )
+
+func TestAbuseIPDBDefaultCredential(t *testing.T) {
+	assert := assert.New(t)
+
+	abuseIPDBCred := AbuseIPDBConnection{
+		ConnectionImpl: ConnectionImpl{
+			HclResourceImpl: modconfig.HclResourceImpl{
+				ShortName: "default",
+			},
+		},
+	}
+
+	os.Unsetenv("ABUSEIPDB_API_KEY")
+	newCreds, err := abuseIPDBCred.Resolve(context.TODO())
+	assert.Nil(err)
+
+	newAbuseIPDBCreds := newCreds.(*AbuseIPDBConnection)
+	assert.Equal("", *newAbuseIPDBCreds.APIKey)
+
+	os.Setenv("ABUSEIPDB_API_KEY", "bfc6f1c42dsfsdfdxxxx26977977b2xxxsfsdda98f313c3d389126de0d")
+
+	newCreds, err = abuseIPDBCred.Resolve(context.TODO())
+	assert.Nil(err)
+
+	newAbuseIPDBCreds = newCreds.(*AbuseIPDBConnection)
+	assert.Equal("bfc6f1c42dsfsdfdxxxx26977977b2xxxsfsdda98f313c3d389126de0d", *newAbuseIPDBCreds.APIKey)
+}
 
 func TestAwsConnection(t *testing.T) {
 
