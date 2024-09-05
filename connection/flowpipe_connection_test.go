24faaa70a3b48890398f3518d7c5e58f25342a9c
Omero Saienni <omero@turbot.com>
2024-09-05 13:12:15 +1200

    .

--- connection/flowpipe_connection_test.go
+++ connection/flowpipe_connection_test.go
@@ -92,3 +92,87 @@ func TestAwsConnection(t *testing.T) {
 	assert.Equal("bar", *newAwsCreds.SecretKey)
 	assert.Nil(newAwsCreds.SessionToken)
 }
+
+func TestAwsConnectionEquals(t *testing.T) {
+	assert := assert.New(t)
+
+	// Case 1: Both connections are nil
+	var conn1 *AwsConnection
+	var conn2 *AwsConnection
+	assert.True(conn1.Equals(conn2), "Both connections should be nil and equal")
+
+	// Case 2: One connection is nil
+	conn1 = &AwsConnection{
+		ConnectionImpl: ConnectionImpl{
+			HclResourceImpl: modconfig.HclResourceImpl{
+				ShortName: "default",
+			},
+		},
+	}
+	assert.False(conn1.Equals(nil), "One connection is nil, should return false")
+
+	// Case 3: Both connections have the same AccessKey, SecretKey, SessionToken, Ttl, and Profile
+	accessKey := "access_key_value"
+	secretKey := "secret_key_value"
+	sessionToken := "session_token_value"
+	ttl := 3600
+	profile := "profile_value"
+
+	conn1 = &AwsConnection{
+		ConnectionImpl: ConnectionImpl{
+			HclResourceImpl: modconfig.HclResourceImpl{
+				ShortName: "default",
+			},
+		},
+		AccessKey:    &accessKey,
+		SecretKey:    &secretKey,
+		SessionToken: &sessionToken,
+		Ttl:          &ttl,
+		Profile:      &profile,
+	}
+
+	conn2 = &AwsConnection{
+		ConnectionImpl: ConnectionImpl{
+			HclResourceImpl: modconfig.HclResourceImpl{
+				ShortName: "default",
+			},
+		},
+		AccessKey:    &accessKey,
+		SecretKey:    &secretKey,
+		SessionToken: &sessionToken,
+		Ttl:          &ttl,
+		Profile:      &profile,
+	}
+
+	assert.True(conn1.Equals(conn2), "Both connections should have the same values and be equal")
+
+	// Case 4: Connections have different AccessKey
+	accessKey2 := "different_access_key"
+	conn2.AccessKey = &accessKey2
+	assert.False(conn1.Equals(conn2), "Connections have different AccessKeys, should return false")
+
+	// Case 5: Connections have different SecretKey
+	conn2.AccessKey = &accessKey // Reset AccessKey to the same
+	secretKey2 := "different_secret_key"
+	conn2.SecretKey = &secretKey2
+	assert.False(conn1.Equals(conn2), "Connections have different SecretKeys, should return false")
+
+	// Case 6: Connections have different Ttl
+	conn2.SessionToken = &sessionToken // Reset SessionToken to the same
+	ttl2 := 7200
+	conn2.Ttl = &ttl2
+	assert.False(conn1.Equals(conn2), "Connections have different Ttl values, should return false")
+
+	// Case 7: Connections have different Profile
+	conn2.Ttl = &ttl // Reset Ttl to the same
+	profile2 := "different_profile"
+	conn2.Profile = &profile2
+	assert.False(conn1.Equals(conn2), "Connections have different Profile values, should return false")
+
+	// NOTE: Finding out why we are not comparing SessionToken
+	// // Case 8: Connections have different SessionToken
+	// conn2.SecretKey = &secretKey // Reset SecretKey to the same
+	// sessionToken2 := "different_session_token"
+	// conn2.SessionToken = &sessionToken2
+	// assert.False(conn1.Equals(conn2), "Connections have different SessionTokens, should return false")
+}
