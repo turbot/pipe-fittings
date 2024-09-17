131a8a6b5199d4181002baf4a4dacead63c6a3e2
kai <kai@turbot.com>
2024-09-17 15:26:49 +0100

    fix customTypeValidationSingle fix mod test suite

--- parse/pipeling_connection.go
+++ parse/pipeling_connection.go
@@ -2,6 +2,7 @@ package parse
 
 import (
 	"fmt"
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/turbot/go-kit/helpers"
--- tests/test_init/app_specific.go
+++ tests/test_init/app_specific.go
@@ -3,6 +3,8 @@ package test_init
 import (
 	"github.com/turbot/go-kit/files"
 	"github.com/turbot/pipe-fittings/app_specific"
+	"github.com/turbot/pipe-fittings/connection"
+	"reflect"
 )
 
 func SetAppSpecificConstants() {
@@ -29,4 +31,39 @@ func SetAppSpecificConstants() {
 	//app_specific.ServiceConnectionAppNamePrefix
 	app_specific.WorkspaceIgnoreFile = ".flowpipeignore"
 	app_specific.WorkspaceDataDir = ".flowpipe"
+	app_specific.ConnectionTypRegistry = map[string]reflect.Type{
+		(&connection.AbuseIPDBConnection{}).GetConnectionType(): reflect.TypeOf(connection.AbuseIPDBConnection{}),
+		(&connection.AlicloudConnection{}).GetConnectionType():  reflect.TypeOf(connection.AlicloudConnection{}),
+		(&connection.AwsConnection{}).GetConnectionType():       reflect.TypeOf(connection.AwsConnection{}),
+		"azure":             reflect.TypeOf(connection.AzureConnection{}),
+		"bitbucket":         reflect.TypeOf(connection.BitbucketConnection{}),
+		"clickup":           reflect.TypeOf(connection.ClickUpConnection{}),
+		"datadog":           reflect.TypeOf(connection.DatadogConnection{}),
+		"discord":           reflect.TypeOf(connection.DiscordConnection{}),
+		"freshdesk":         reflect.TypeOf(connection.FreshdeskConnection{}),
+		"gcp":               reflect.TypeOf(connection.GcpConnection{}),
+		"github":            reflect.TypeOf(connection.GithubConnection{}),
+		"gitlab":            reflect.TypeOf(connection.GitLabConnection{}),
+		"ip2locationio":     reflect.TypeOf(connection.IP2LocationIOConnection{}),
+		"ipstack":           reflect.TypeOf(connection.IPstackConnection{}),
+		"jira":              reflect.TypeOf(connection.JiraConnection{}),
+		"jumpcloud":         reflect.TypeOf(connection.JumpCloudConnection{}),
+		"mastodon":          reflect.TypeOf(connection.MastodonConnection{}),
+		"microsoft_teams":   reflect.TypeOf(connection.MicrosoftTeamsConnection{}),
+		"okta":              reflect.TypeOf(connection.OktaConnection{}),
+		"openai":            reflect.TypeOf(connection.OpenAIConnection{}),
+		"opsgenie":          reflect.TypeOf(connection.OpsgenieConnection{}),
+		"pagerduty":         reflect.TypeOf(connection.PagerDutyConnection{}),
+		"sendgrid":          reflect.TypeOf(connection.SendGridConnection{}),
+		"servicenow":        reflect.TypeOf(connection.ServiceNowConnection{}),
+		"slack":             reflect.TypeOf(connection.SlackConnection{}),
+		"trello":            reflect.TypeOf(connection.TrelloConnection{}),
+		"turbot_guardrails": reflect.TypeOf(connection.GuardrailsConnection{}),
+		"turbot_pipes":      reflect.TypeOf(connection.PipesConnection{}),
+		"uptime_robot":      reflect.TypeOf(connection.UptimeRobotConnection{}),
+		"urlscan":           reflect.TypeOf(connection.UrlscanConnection{}),
+		"vault":             reflect.TypeOf(connection.VaultConnection{}),
+		"virus_total":       reflect.TypeOf(connection.VirusTotalConnection{}),
+		"zendesk":           reflect.TypeOf(connection.ZendeskConnection{}),
+	}
 }
