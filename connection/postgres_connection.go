7429afb9637b774bafd799f89eed5e0b8e5ffbb5
kai <kai@turbot.com>
2024-09-18 09:13:44 +0100

    working on query step database param

--- connection/postgres_connection.go
+++ connection/postgres_connection.go
@@ -1,13 +1,72 @@
 package connection
 
-import "github.com/turbot/pipe-fittings/modconfig"
+import (
+	"context"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/turbot/go-kit/helpers"
+	"github.com/turbot/pipe-fittings/modconfig"
+	"github.com/turbot/pipe-fittings/utils"
+	"github.com/zclconf/go-cty/cty"
+)
 
 type PostgresConnection struct {
 	modconfig.ConnectionImpl
-	UserName   *string `json:"username,omitempty" cty:"username" hcl:"username,optional"`
-	Host       *string `json:"host,omitempty" cty:"host" hcl:"host,optional"`
-	Port       *int    `json:"port,omitempty" cty:"port" hcl:"port,optional"`
-	Database   *string `json:"database,omitempty" cty:"database" hcl:"database,optional"`
-	Password   *string `json:"password,omitempty" cty:"password" hcl:"password,optional"`
-	SearchPath *string `json:"search_path,omitempty" cty:"search_path" hcl:"search_path,optional"`
+	UserName         *string `json:"username,omitempty" cty:"username" hcl:"username,optional"`
+	Host             *string `json:"host,omitempty" cty:"host" hcl:"host,optional"`
+	Port             *int    `json:"port,omitempty" cty:"port" hcl:"port,optional"`
+	ConnectionString *string `json:"database,omitempty" cty:"database" hcl:"database,optional"`
+	Password         *string `json:"password,omitempty" cty:"password" hcl:"password,optional"`
+	SearchPath       *string `json:"search_path,omitempty" cty:"search_path" hcl:"search_path,optional"`
+}
+
+func (p *PostgresConnection) GetConnectionType() string {
+	return "postgres"
+}
+
+func (p *PostgresConnection) Resolve(ctx context.Context) (modconfig.PipelingConnection, error) {
+	// if we have a connection string, return it as is
+	if p.ConnectionString != nil {
+		return p, nil
+	}
+
+	// TODO build a connection string from the other fields
+	panic("implement me")
+
+}
+
+func (p *PostgresConnection) GetTtl() int {
+	return -1
+}
+
+func (p *PostgresConnection) Validate() hcl.Diagnostics {
+	return hcl.Diagnostics{}
+}
+
+func (p *PostgresConnection) GetEnv() map[string]cty.Value {
+	// TODO POSTGRES ENV
+	return map[string]cty.Value{}
+}
+
+func (p *PostgresConnection) Equals(otherConnection modconfig.PipelingConnection) bool {
+	// If both pointers are nil, they are considered equal
+	if p == nil && helpers.IsNil(otherConnection) {
+		return true
+	}
+
+	if (p == nil && !helpers.IsNil(otherConnection)) || (p != nil && helpers.IsNil(otherConnection)) {
+		return false
+	}
+
+	other, ok := otherConnection.(*PostgresConnection)
+	if !ok {
+		return false
+	}
+
+	return utils.PtrEqual(p.UserName, other.UserName) &&
+		utils.PtrEqual(p.Host, other.Host) &&
+		utils.PtrEqual(p.Port, other.Port) &&
+		utils.PtrEqual(p.ConnectionString, other.ConnectionString) &&
+		utils.PtrEqual(p.Password, other.Password) &&
+		utils.PtrEqual(p.SearchPath, other.SearchPath)
+
 }
--- modconfig/flowpipe_pipeline_step_query.go
+++ modconfig/flowpipe_pipeline_step_query.go
@@ -1,6 +1,8 @@
 package modconfig
 
 import (
+	"strings"
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/gohcl"
 	"github.com/turbot/go-kit/helpers"
@@ -10,6 +12,7 @@ import (
 	"github.com/turbot/pipe-fittings/schema"
 	"github.com/turbot/pipe-fittings/utils"
 	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty/gocty"
 )
 
 type PipelineStepQuery struct {
@@ -66,10 +69,41 @@ func (p *PipelineStepQuery) GetInputs(evalContext *hcl.EvalContext) (map[string]
 	}
 
 	// database
-	results, diags = simpleTypeInputFromAttribute(p.GetUnresolvedAttributes(), results, evalContext, schema.AttributeTypeDatabase, p.Database)
-	if diags.HasErrors() {
-		return nil, error_helpers.BetterHclDiagsToError(p.Name, diags)
+	if databaseExpression, ok := p.UnresolvedAttributes[schema.AttributeTypeDatabase]; ok {
+		// attribute needs resolving, this case may happen if we specify the entire option as an attribute
+		var connValue cty.Value
+		diags := gohcl.DecodeExpression(databaseExpression, evalContext, &connValue)
+		if diags.HasErrors() {
+			return nil, error_helpers.BetterHclDiagsToError(p.Name, diags)
+		}
+		c, err := CtyValueToConnection(connValue)
+		if err != nil {
+			return nil, perr.BadRequestWithMessage(p.Name + ": unable to resolve connection attribute: " + err.Error())
+		}
+		results[schema.AttributeTypeDatabase] = c
+
+	} else {
+		// database
+		results, diags = simpleTypeInputFromAttribute(p.GetUnresolvedAttributes(), results, evalContext, schema.AttributeTypeDatabase, p.Database)
+		if diags.HasErrors() {
+			return nil, error_helpers.BetterHclDiagsToError(p.Name, diags)
+		}
 	}
+
+	//// attribute needs resolving, this case may happen if we specify the entire option as an attribute
+	//var opts cty.Value
+	//diags := gohcl.DecodeExpression(p.UnresolvedAttributes[schema.AttributeTypeOptions], evalContext, &opts)
+	//if diags.HasErrors() {
+	//	return nil, error_helpers.BetterHclDiagsToError(p.Name, diags)
+	//}
+	//resolvedOpts, err = CtyValueToPipelineStepInputOptionList(opts)
+	//if err != nil {
+	//	return nil, perr.BadRequestWithMessage(p.Name + ": unable to parse options attribute: " + err.Error())
+	//}
+	//results, diags = simpleTypeInputFromAttribute(p.GetUnresolvedAttributes(), results, evalContext, schema.AttributeTypeDatabase, p.Database)
+	//if diags.HasErrors() {
+	//	return nil, error_helpers.BetterHclDiagsToError(p.Name, diags)
+	//}
 
 	if _, ok := results[schema.AttributeTypeDatabase]; !ok {
 		return nil, perr.BadRequestWithMessage(p.Name + ": database must be supplied")
@@ -147,3 +181,81 @@ func (p *PipelineStepQuery) Validate() hcl.Diagnostics {
 	diags := p.ValidateBaseAttributes()
 	return diags
 }
+
+func CtyValueToConnection(value cty.Value) (_ PipelingConnection, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = perr.BadRequestWithMessage("unable to decode connection: " + r.(string))
+		}
+	}()
+	// get the name and extract the block type
+	name := value.GetAttr("name")
+
+	parts := strings.Split(name.AsString(), ".")
+	if len(parts) != 2 {
+		return nil, perr.BadRequestWithMessage("connection name must be in the format <type>.<name>")
+	}
+	connectionType := parts[0]
+
+	// now instantiate an empty connection of the correct type
+	conn, err := instantiateConnection(connectionType, HclResourceImpl{})
+	if err != nil {
+		return nil, perr.BadRequestWithMessage("unable to decode connection: " + err.Error())
+	}
+	// now decode the cty value into the connection
+	err = gocty.FromCtyValue(value, &conn)
+	if err != nil {
+		return nil, perr.BadRequestWithMessage("unable to decode connection: " + err.Error())
+	}
+	return conn, nil
+}
+
+//	var output []PipelineStepInputOption
+//
+//	opts := value.AsValueSlice()
+//
+//	for _, opt := range opts {
+//		valueMap := opt.AsValueMap()
+//
+//		isValid := false
+//		option := PipelineStepInputOption{
+//			UnresolvedAttributes: make(map[string]hcl.Expression),
+//		}
+//
+//		for k, v := range valueMap {
+//			switch k {
+//			case schema.AttributeTypeValue:
+//				if !v.IsNull() {
+//					isValid = true
+//					val := v.AsString()
+//					option.Value = &val
+//				}
+//			case schema.AttributeTypeLabel:
+//				if !v.IsNull() {
+//					label := v.AsString()
+//					option.Label = &label
+//				}
+//			case schema.AttributeTypeSelected:
+//				if !v.IsNull() && v.Type() == cty.Bool {
+//					isSelected := v.True()
+//					option.Selected = &isSelected
+//				}
+//			case schema.AttributeTypeStyle:
+//				if !v.IsNull() {
+//					s := v.AsString()
+//					option.Style = &s
+//				}
+//			default:
+//				return nil, perr.BadRequestWithMessage(k + " is not a valid attribute for input options")
+//			}
+//		}
+//
+//		if isValid {
+//			output = append(output, option)
+//		} else {
+//			return nil, perr.BadRequestWithMessage("input options must declare a value")
+//		}
+//	}
+//
+//	return output, nil
+//}
