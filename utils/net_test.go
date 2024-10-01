51cc430bef3442bb39d9643a29ba120303962359
Victor Hadianto <victor@turbot.com>
2024-10-01 14:32:50 +1000

    Update TestIsPortBindable to be more resilient when running in local machine.

--- utils/net_test.go
+++ utils/net_test.go
@@ -7,21 +7,24 @@ import (
 	"testing"
 )
 
-// TestIsPortBindable tests the IsPortBindable function - assumes that the 8080 port is not in use
+// TestIsPortBindable tests the IsPortBindable function - assumes that the 9783 port is not in use
+//
+// Updated test to use random port 9783 rather than very commonly used 8080 that often cause
+// this test to fail because there will be something running in 8080
 func TestIsPortBindable(t *testing.T) {
 	// Test case 1: Port is bindable
-	err := IsPortBindable("localhost", 8080)
+	err := IsPortBindable("localhost", 9783)
 	if err != nil {
 		t.Errorf("Expected no error, but got: %v", err)
 	}
 
-	ln, err := net.Listen("tcp", net.JoinHostPort("localhost", "8080"))
+	ln, err := net.Listen("tcp", net.JoinHostPort("localhost", "9783"))
 	if err != nil {
 		t.Fatalf("Expected no error, but got: %v", err)
 	}
 	defer ln.Close()
 	// Test case 2: Port is already in use
-	err = IsPortBindable("localhost", 8080)
+	err = IsPortBindable("localhost", 9783)
 	if err == nil {
 		t.Error("Expected an error, but got nil")
 	} else {
