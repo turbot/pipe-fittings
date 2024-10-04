4bc58d551a14836fc4284d79c82573cda5ce1a41
Puskar Basu <puskar@turbot.com>
2024-10-04 13:58:54 +0530

    update DefaultSteampipeWorkspaceContent

--- constants/default_workspaces.go
+++ constants/default_workspaces.go
@@ -13,13 +13,11 @@ const DefaultSteampipeWorkspaceContent = `
 #   pipes_host         = "pipes.turbot.com"
 #   pipes_token        = "spt_999faketoken99999999_111faketoken1111111111111"
 #   install_dir        = "~/steampipe2"
-#   mod_location       = "~/src/steampipe-mod-aws-insights"  
 #   query_timeout      = 300
 #   snapshot_location  = "acme/dev"
 #   workspace_database = "local" 
 #   search_path        = "aws,aws_1,aws_2,gcp,gcp_1,gcp_2,slack,github"
 #   search_path_prefix = "aws_all"
-#   watch              = true
 #   max_parallel       = 5
 #   introspection      = false
 #   input              = true
@@ -36,17 +34,6 @@ const DefaultSteampipeWorkspaceContent = `
 #     output       = "table" # json, csv, table, line
 #     separator    = ","     # any single char
 #     timing       = on   # off, on, verbose
-#   }
-# 
-#   options "check" {
-#     header    = true    # true, false
-#     output    = "text"  # brief, csv, html, json, md, text, snapshot or none (default "text")
-#     separator = ","     # any single char
-#     timing    = true    # true, false
-#   }
-#   
-#   options "dashboard" {
-#     browser = true
 #   }
 # }
 `
