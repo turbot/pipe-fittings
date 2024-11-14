083e01d072c5aca6d20b6af50971af6cfd16d2b8
kai <kai@turbot.com>
2024-11-14 12:59:09 +0700

    Add InstanceOf

--- utils/reflect.go
+++ utils/reflect.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -10,4 +11,17 @@ func GetGenericTypeName[T any]() string {
 	longName := fmt.Sprintf("%T", *new(T))
 	split := strings.Split(longName, ".")
 	return split[len(split)-1]
+}
+
+func InstanceOf[T any]() T {
+	var x T
+	// is x a pointee
+	if reflect.ValueOf(x).Kind() != reflect.Ptr {
+		return x
+	}
+	var y interface{} = x
+	rv := reflect.ValueOf(y)
+	t := rv.Type().Elem()
+	y = reflect.New(t).Interface()
+	return y.(T)
 }
