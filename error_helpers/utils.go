13301a1be75ac145cd926ee87b143ba11611fcb1
kai <kai@turbot.com>
2024-12-11 13:44:34 +0000

    Update TransformErrorToSteampipe to handle RPC errors better

--- error_helpers/utils.go
+++ error_helpers/utils.go
@@ -79,12 +79,11 @@ func TransformErrorToSteampipe(err error) error {
 	// an error that originated from our database/sql driver (always prefixed with "ERROR:")
 	if strings.HasPrefix(errString, "ERROR:") {
 		errString = strings.TrimSpace(strings.TrimPrefix(errString, "ERROR:"))
-
-		// if this is an RPC Error while talking with the plugin
-		if strings.HasPrefix(errString, "rpc error") {
-			// trim out "rpc error: code = Unknown desc ="
-			errString = strings.TrimPrefix(errString, "rpc error: code = Unknown desc =")
-		}
+	}
+	// if this is an RPC Error while talking with the plugin
+	if strings.HasPrefix(errString, "rpc error") {
+		// trim out "rpc error: code = Unknown desc ="
+		errString = strings.TrimPrefix(errString, "rpc error: code = Unknown desc =")
 	}
 	return errors.New(strings.TrimSpace(errString))
 }
