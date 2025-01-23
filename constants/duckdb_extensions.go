3fc71f5c225319365769d09da414a00fa8196a54
Puskar Basu <puskar@turbot.com>
2025-01-23 12:35:57 +0530

    Install and load duckdb extensions from pipes extensions dir

---
+++ constants/duckdb_extensions.go
@@ -0,0 +1,5 @@
+package constants
+
+// extend this list to include any additional extensions that need to be installed and loaded
+// into the duckdb backend for Powerpipe and Flowpipe
+var DuckDbExtensions = []string{"json"}
