6a911f03f8cbe7b0b6e4f364b746944a8d51c5be
kai <kai@turbot.com>
2025-03-17 15:35:42 +0000

    rename plugin.Exists to ExistsInVersionFile

--- plugin/exists.go
+++ plugin/exists.go
@@ -2,21 +2,21 @@ package plugin
 
 import (
 	"context"
-
 	"github.com/turbot/pipe-fittings/v2/ociinstaller"
 	"github.com/turbot/pipe-fittings/v2/versionfile"
 )
 
-// Exists looks up the version file and reports whether a plugin is already installed
-func Exists(ctx context.Context, plugin string) (bool, error) {
+// ExistsInVersionFile looks up the version file and reports whether a plugin is already installed
+func ExistsInVersionFile(ctx context.Context, plugin string) (bool, error) {
 	versionData, err := versionfile.LoadPluginVersionFile(ctx)
 	if err != nil {
 		return false, err
 	}
 
-	imageRef := ociinstaller.NewImageRef(plugin)
+	imageRef := ociinstaller.NewImageRef(plugin).DisplayImageRef()
 
 	// lookup in the version data
-	_, found := versionData.Plugins[imageRef.DisplayImageRef()]
+	_, found := versionData.Plugins[imageRef]
+
 	return found, nil
 }
