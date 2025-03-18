2a0fa785cd36beb75aa17807a7ac5afbeabef9c2
kai <kai@turbot.com>
2025-03-18 13:59:42 +0000

    Revert "rename plugin.Exists to ExistsInVersionFile"
    This reverts commit 6a911f03f8cbe7b0b6e4f364b746944a8d51c5be.

--- plugin/exists.go
+++ plugin/exists.go
@@ -2,21 +2,21 @@ package plugin
 
 import (
 	"context"
+
 	"github.com/turbot/pipe-fittings/v2/ociinstaller"
 	"github.com/turbot/pipe-fittings/v2/versionfile"
 )
 
-// ExistsInVersionFile looks up the version file and reports whether a plugin is already installed
-func ExistsInVersionFile(ctx context.Context, plugin string) (bool, error) {
+// Exists looks up the version file and reports whether a plugin is already installed
+func Exists(ctx context.Context, plugin string) (bool, error) {
 	versionData, err := versionfile.LoadPluginVersionFile(ctx)
 	if err != nil {
 		return false, err
 	}
 
-	imageRef := ociinstaller.NewImageRef(plugin).DisplayImageRef()
+	imageRef := ociinstaller.NewImageRef(plugin)
 
 	// lookup in the version data
-	_, found := versionData.Plugins[imageRef]
-
+	_, found := versionData.Plugins[imageRef.DisplayImageRef()]
 	return found, nil
 }
