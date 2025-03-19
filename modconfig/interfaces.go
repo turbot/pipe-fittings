923f9d1b007f82ad590272c5072ee764b3a73079
kaidaguerre <kai@turbot.com>
2025-03-19 10:39:06 +0000

    Add AppSpecificParseResourceNameFunc to provide a mechanism to override the resource name parser used by DecodeHclBody - set by Tailpipe to use its own ParsedResourceName type (#662)
    * rename plugin.Exists to ExistsInVersionFile
    
    * Add ResourceNameProvider interface to abstract away ParsedResourceName
    Update ModResources, ModResourcesProvider and ResourceProvider - GetResource now accepts ResourceNameProvider
    Add app_specific.ParseResourceName to allow Tailpipe to use its own ParsedResourceName for DecodeHclBody
    
    * Improve comments
    
    * Revert "rename plugin.Exists to ExistsInVersionFile"
    
    This reverts commit 6a911f03f8cbe7b0b6e4f364b746944a8d51c5be.
    
    * remove unused function getPropertiesFromContent
    
    * Add AppSpecificParseResourceNameFunc to provide a mechanism to override the resource name parser used by DecodeHclBody - set by Tailpipe to use its own ParsedResourceName type

--- modconfig/interfaces.go
+++ modconfig/interfaces.go
@@ -77,7 +77,7 @@ type ResourceWithMetadata interface {
 type ModResources interface {
 	WalkResources(resourceFunc func(item HclResource) (bool, error)) error
 	AddResource(item HclResource) hcl.Diagnostics
-	GetResource(parsedName *ParsedResourceName) (resource HclResource, found bool)
+	GetResource(parsedName ResourceNameProvider) (resource HclResource, found bool)
 	Equals(other ModResources) bool
 	AddReference(ref *ResourceReference)
 	GetReferences() map[string]*ResourceReference
@@ -89,9 +89,13 @@ type ModResources interface {
 
 type ModResourcesProvider interface {
 	GetModResources() ModResources
-	GetResource(parsedName *ParsedResourceName) (resource HclResource, found bool)
+	GetResource(parsedName ResourceNameProvider) (resource HclResource, found bool)
 }
 
 type ResourceProvider interface {
-	GetResource(parsedName *ParsedResourceName) (resource HclResource, found bool)
+	GetResource(parsedName ResourceNameProvider) (resource HclResource, found bool)
+}
+
+type ResourceNameProvider interface {
+	ToResourceName() string
 }
