fa03c15da49f76a6f9ded9680c0308e1a25577e7
kaidaguerre <kai@turbot.com>
2025-03-20 16:31:20 +0000

    reinstate legacy grok auto escaping - but display warning if a property needs to be escaped (#665)
    * reinstate legacy grok auto escaping - but display warning if a property needs to be escaped

--- parse/parser_opts.go
+++ parse/parser_opts.go
@@ -8,7 +8,7 @@ type ParseHclOpt func(*ParseHclConfig)
 
 // WithDisableTemplateForProperties is an option to specify the properties for which
 // hcl template expression parsing should be disabled
-// this is used for preprties which will include a grok path which include the template opening chars '%{'
+// this is used for properties which will include a grok path which include the template opening chars '%{'
 // if we do not escape the '%{' for these properties, they will fail ot parse
 func WithDisableTemplateForProperties(properties []string) ParseHclOpt {
 	return func(c *ParseHclConfig) {
@@ -18,8 +18,8 @@ func WithDisableTemplateForProperties(properties []string) ParseHclOpt {
 
 // WithEscapeBackticks is an option to specify whether the grok function should be applied to the hcl file
 // this is used to escape grok expressions in the hcl file
-func WithEscapeBackticks(applyGrokFunction bool) ParseHclOpt {
+func WithEscapeBackticks(escapeBackticks bool) ParseHclOpt {
 	return func(c *ParseHclConfig) {
-		c.escapeBackticks = applyGrokFunction
+		c.escapeBackticks = escapeBackticks
 	}
 }
