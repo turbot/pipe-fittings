ed7e1818c0c2c3ebb83d0067a006a50a765f081f
Puskar Basu <45908484+pskrbasu@users.noreply.github.com>
2025-03-24 15:44:27 +0530

    Add CSV and JSON exporters for powerpipe query run export. Closes #651

---
+++ export/csv_exporter.go
@@ -0,0 +1,37 @@
+package export
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/turbot/pipe-fittings/v2/constants"
+	"github.com/turbot/pipe-fittings/v2/querydisplay"
+	"github.com/turbot/pipe-fittings/v2/queryresult"
+)
+
+type CsvExporter struct {
+	ExporterBase
+}
+
+// Export processes the query result and writes CSV output to a file
+func (e *CsvExporter) Export(ctx context.Context, input ExportSourceData, filePath string) error {
+	result, ok := input.(*queryresult.Result[*queryresult.QueryTimingMetadata])
+	if !ok {
+		return fmt.Errorf("CsvExporter input must be a queryresult.Result")
+	}
+
+	// Generate csv output
+	jsonString, _, _ := querydisplay.BuildCSV(ctx, result)
+
+	// Write to file
+	return Write(filePath, strings.NewReader(jsonString))
+}
+
+func (e *CsvExporter) FileExtension() string {
+	return constants.CsvExtension
+}
+
+func (e *CsvExporter) Name() string {
+	return constants.OutputFormatCSV
+}
