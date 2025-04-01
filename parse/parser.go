1caf03b2ed1af7a30d07b0de72d75e24d23c6c0f
kai <kai@turbot.com>
2025-04-01 17:09:18 +0100

    tidy

--- parse/parser.go
+++ parse/parser.go
@@ -2,16 +2,17 @@ package parse
 
 import (
 	"fmt"
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclparse"
-	"github.com/hashicorp/hcl/v2/json"
-	"github.com/turbot/pipe-fittings/v2/app_specific"
-	"github.com/turbot/pipe-fittings/v2/constants"
 	"io"
 	"os"
 	"path/filepath"
 	"sigs.k8s.io/yaml"
 	"sort"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclparse"
+	"github.com/hashicorp/hcl/v2/json"
+	"github.com/turbot/pipe-fittings/v2/app_specific"
+	"github.com/turbot/pipe-fittings/v2/constants"
 )
 
 // LoadFileData builds a map of filepath to file data
