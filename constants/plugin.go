a3fe2192f71c3e9c470f73bc017d621a659ac8f1
Priyanka Chatterjee <priyanka.chatterjee@turbot.com>
2025-08-30 01:34:25 +0530

    Add new install message for plugins not distributed via hub.tailpipe.io

--- constants/plugin.go
+++ constants/plugin.go
@@ -4,6 +4,7 @@ const (
 	InstallMessagePluginAlreadyInstalled       = "Already installed"
 	InstallMessagePluginLatestAlreadyInstalled = "Latest already installed"
 	InstallMessagePluginNotInstalled           = "Not installed"
+	InstallMessagePluginNotDistributedViaHub   = "Not distributed via hub.tailpipe.io"
 	InstallMessagePluginNotFound               = "Not found"
 	ConnectionErrorPluginFailedToStart         = "plugin failed to start"
 	ConnectionErrorPluginNotInstalled          = "plugin not installed"
